8465574ba755bf6244ada3dbc9f74569229ee61c
Matt Henry <matt.henry@owensboro.kyschools.us>
2014-02-15 09:29:21 -0800

    Installed Guard SPork, Changed versions of Gems Got it working

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
-describe "Static pages" do
-  let(:base_title){ "Ruby on Rails Tutorial Sample App | " }
-  describe "Home page" do
+describe 'Static Pages' do
+  let(:base_title){ 'Ruby on Rails Tutorial Sample App | ' }
+  describe 'Home page' do
 
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
@@ -15,7 +15,7 @@ describe "Static pages" do
     end
   end
 
-  describe "Help page" do
+  describe 'Help page' do
 
     it "should have the content 'Help'" do
       visit '/static_pages/help'
@@ -28,7 +28,7 @@ describe "Static pages" do
     end
   end
 
-  describe "About page" do
+  describe 'About page' do
 
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
@@ -41,7 +41,7 @@ describe "Static pages" do
     end
   end
 
-  describe "Contact page" do
+  describe 'Contact page' do
     it "should have the content 'Contact Us'" do
       visit '/static_pages/contact'
       expect(page).to have_content('Contact Us')
@@ -49,7 +49,7 @@ describe "Static pages" do
 
     it "should have title 'Contact Us'" do
       visit '/static_pages/contact'
-      expect(page).to have_title("#{base_title}Contact Us");
+      expect(page).to have_title("#{base_title}Contact Us")
     end
   end
 end
