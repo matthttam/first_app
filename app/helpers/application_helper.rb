421c654887ad7ece9260f780f5ec629b043f02ca
Matt Henry <matt.henry@owensboro.kyschools.us>
2014-02-15 23:28:11 -0800

    Finished Chapter 5 - Layout and routes.  Made my own custom template for Bootstrap 3 and made an active header link helper class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,18 @@ module ApplicationHelper
       "#{base_title} | #{page_title}"
     end
   end
+
+  # returns list element of active class or not with link inside.
+  def active_list_link_to(text, path )
+   list_class = ((path == request.env['PATH_INFO']) ? ' class="active" ' : nil)
+   ("<li#{list_class}>" + (link_to text, path) + "</li>").html_safe
+  end
 end
+#Check name of controller
+#def controller?(*controller)
+#  controller.include?(params[:controller])
+#end
+#Check name of action
+#def action?(*action)
+#  action.include?(params[:action])
+#end
